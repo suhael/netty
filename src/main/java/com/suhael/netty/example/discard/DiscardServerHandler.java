95d9c141fab5a09c4e9da761144f349fdc548c1c
Suhael Akhtar <suhael.akhtar@maersk.com>
2016-08-17 15:30:55 +0100

    echo data

--- src/main/java/com/suhael/netty/example/discard/DiscardServerHandler.java
+++ src/main/java/com/suhael/netty/example/discard/DiscardServerHandler.java
@@ -1,10 +1,7 @@
 package com.suhael.netty.example.discard;
 
-import io.netty.buffer.ByteBuf;
-
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
-import io.netty.util.ReferenceCountUtil;
 
 /**
  * Handles a server-side channel.
@@ -13,15 +10,8 @@ public class DiscardServerHandler extends ChannelInboundHandlerAdapter { // (1)
 
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) {
-        ByteBuf in = (ByteBuf) msg;
-        try {
-            while (in.isReadable()) { // (1)
-                System.out.print((char) in.readByte());
-                System.out.flush();
-            }
-        } finally {
-            ReferenceCountUtil.release(msg); // (2)
-        }
+        ctx.write(msg); // (1)
+        ctx.flush(); // (2)
     }
 
     @Override
